3170d58242e6f153d760a16f6331ed20101d38e9
GaryQuan <garylyq17@gmail.com>
2019-08-29 16:38:31 +0800

    Added AddBtn & AddLogModal Component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import AddBtn from './components/layout/AddBtn';
 
 import Logs from './components/logs/Logs';
 import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+
+import TechListModal from './components/techs/TechListModal';
+import AddTechModal from './components/techs/AddTechModal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -22,6 +26,9 @@ const App = () => {
       <div className='container'>
         <AddBtn />
         <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </Fragment>
