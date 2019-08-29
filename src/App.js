dff9667fc32f5f307726b550c17147fa52ffc298
GaryQuan <garylyq17@gmail.com>
2019-08-29 17:03:11 +0800

    Setting up Redux Store & Provider

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import EditLogModal from './components/logs/EditLogModal';
 import TechListModal from './components/techs/TechListModal';
 import AddTechModal from './components/techs/AddTechModal';
 
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
@@ -21,17 +24,19 @@ const App = () => {
   }, []);
 
   return (
-    <Fragment>
-      <SearchBar />
-      <div className='container'>
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
 };
 
