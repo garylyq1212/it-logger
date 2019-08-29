62024f2f7dcdcbc316f2f950fb6f749b2da57527
GaryQuan <garylyq17@gmail.com>
2019-08-29 22:19:40 +0800

    Added searchLogs in Redux reducers, actions & connect to SearchBar Component

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
 
-const SearchBar = () => {
+import { connect } from 'react-redux';
+import { searchLogs } from '../../redux/actions/logActions';
+
+const SearchBar = ({ searchLogs }) => {
+  const text = useRef('');
+
+  const onChange = e => {
+    searchLogs(text.current.value);
+  };
+
   return (
     <nav style={{ marginBottom: '30px' }} className='blue'>
       <div className='nav-wrapper'>
         <form>
           <div className='input-field'>
-            <input id='search' type='search' />
+            <input
+              id='search'
+              type='search'
+              placeholder='Search Logs...'
+              ref={text}
+              onChange={onChange}
+            />
             <label className='label-icon' htmlFor='search'>
               <i className='material-icons'>search</i>
             </label>
@@ -18,4 +34,15 @@ const SearchBar = () => {
   );
 };
 
-export default SearchBar;
+SearchBar.propTypes = {
+  searchLogs: PropTypes.func.isRequired
+};
+
+const mapDispatchtoProps = {
+  searchLogs
+};
+
+export default connect(
+  null,
+  mapDispatchtoProps
+)(SearchBar);
