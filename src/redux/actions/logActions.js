bd0c04a46d77cbcd0f7ba8ddf69b8376bd64f3ad
GaryQuan <garylyq17@gmail.com>
2019-08-29 17:55:13 +0800

    Added Logs Reducer, Actions & Types

---
+++ src/redux/actions/logActions.js
@@ -0,0 +1,41 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERRORS } from './types';
+
+// export const getLogs = () => {
+//   return async dispatch => {
+//     setLoading();
+
+//     const res = await fetch('/logs');
+//     const data = await res.json();
+
+//     dispatch({
+//       type: GET_LOGS,
+//       payload: data
+//     });
+//   };
+// };
+
+export const getLogs = () => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch('/logs');
+    const data = await res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERRORS,
+      payload: err.response.data
+    });
+  }
+};
+
+// Set loading
+export const setLoading = () => {
+  return {
+    type: SET_LOADING
+  };
+};
