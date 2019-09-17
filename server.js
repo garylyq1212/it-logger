a6fc574138f054d591929b8e59c9a01f46da9c7d
GaryQuan <garylyq17@gmail.com>
2019-09-17 17:54:18 +0800

    Serve static assets in production & Add heroku config script

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const jsonServer = require('json-server');
+const path = require('path');
+
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+
 const port = process.env.PORT || 5000;
 
 server.use(middlewares);
 server.use(router);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  server.use(jsonServer.defaults({ static: 'client/build' }));
+}
 
 server.listen(port);
