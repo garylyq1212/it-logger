0c7cc8d8e3305c9e1c54da6dee3146229136e75a
GaryQuan <garylyq17@gmail.com>
2019-09-17 17:57:46 +0800

    Revert "Serve static assets in production & Add heroku config script"
    This reverts commit a6fc574138f054d591929b8e59c9a01f46da9c7d.

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
 const jsonServer = require('json-server');
-const path = require('path');
-
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-
 const port = process.env.PORT || 5000;
 
 server.use(middlewares);
 server.use(router);
-
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  server.use(jsonServer.defaults({ static: 'client/build' }));
-}
 
 server.listen(port);
