532d64c233a3f321678c39706087461be321df3a
GaryQuan <garylyq17@gmail.com>
2019-09-17 18:01:57 +0800

    Deploy to Heroku

---
+++ public/precache-manifest.b1bba1bf2b03dbe8c0c5dd912d642e0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c1b3f7ae84eb47b029465a3522aec18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f6debc8b73165d3fe02",
+    "url": "/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "e197c690d21a272ff96a",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "0f6debc8b73165d3fe02",
+    "url": "/static/js/2.78a1f2e8.chunk.js"
+  },
+  {
+    "revision": "e197c690d21a272ff96a",
+    "url": "/static/js/main.a7d0b962.chunk.js"
+  },
+  {
+    "revision": "79d5aaaaeda58b4c93a3",
+    "url": "/static/js/runtime~main.74c50cc8.js"
+  }
+]);
